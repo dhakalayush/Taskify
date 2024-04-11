0617381fab49dba190a02ac702de9d519de68de3
RaghavSitaula <113698501+RaghavSitaula@users.noreply.github.com>
2024-04-11 23:46:15 +0545

    login

---
+++ frontend_taskify/src/app/(auth)/login/page.js
@@ -0,0 +1,95 @@
+"use client"; // This is a client component
+
+import styles from "./page.module.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+const Home = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8081/login", { username, password })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+  return (
+    <div className={styles.main}>
+      {/* Left page */}
+      <div className={styles.main2}>
+        <h1 className={styles.left_head}>
+          Organize your work and life,finally
+        </h1>
+        <p className={styles.left_btm}>
+          Become focused,organized, and calm with TASKify.
+          <br />
+          The world's #1 user-friendly Task Manager.
+        </p>
+      </div>
+
+      {/* Right page */}
+      <div className={styles.main4}>
+        <h1 className={styles.logo}>TASKify</h1>
+        <div className={styles.form}>
+          <h1 className={styles.fmhd}>LOG IN</h1>
+          <h3 className={styles.heading}>
+            Use one of the services to continue with TASKify
+          </h3>
+
+          {/* Username Input */}
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            {/* Password Input */}
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {/* Login Button */}
+            <button type="submit" className={styles.pbtn}>
+              LOGIN
+            </button>
+            <button className={styles.pbtn}>
+              <a href="/dashboard" className={styles.lo}>
+                LOGIN
+              </a>
+            </button>
+          </form>
+
+          <span className={styles.or}>OR </span>
+
+          {/* Continue with Mail Button */}
+          <button className={styles.sbtn}>Continue with Mail</button>
+
+          {/* Sign Up Link */}
+          <p>
+            Don't have an account? <a href="/">Sign up</a>
+          </p>
+        </div>
+
+        {/* Terms and Privacy */}
+        <p className={styles.btmtxt}>
+          By continuing, you agree to TASKify's <u>Terms of Use</u>
+          <br />
+          Read our <u>Privacy Policy</u>.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Home;
